d28484c644547a6685595a0bc9b6a3ee294f12af
Neosky <her_liu@qq.com>
2020-01-22 14:12:37 +0800

    1

--- index.js
+++ index.js
@@ -11,9 +11,7 @@ const instance = axios.create({
 (async () => {
     try {
         let secret = '1417856569'
-        // let { data } = await instance.get('/user/repos')  
         let { data } = await instance.post('/repos/liuzemei/test2/hooks', {
-            // let { data } = await instance.patch('/repos/liuzemei/git_hook_test/hooks/176880231', {
             config: {
                 url: 'http://118.24.121.133:9089/webhook',
                 content_type: 'json',
