56b0863b0bebef67e0cf442afb43025a3213c772
Neosky <her_liu@qq.com>
2020-01-22 12:14:14 +0800

    1

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const axios = require('axios')
 const instance = axios.create({
     baseURL: 'https://api.github.com',
     headers: {
-        Authorization: 'token fc747a7121a433bd45593cf4299e6c6c276809a3',
+        Authorization: 'token c1691741b7a644bed537733fb58f67152d4ae04e',
         'Content-Type': 'application/json; charset=utf-8'
     }
 });1
@@ -15,8 +15,9 @@ const instance = axios.create({
         // let { data } = await instance.post('/repos/liuzemei/git_hook_test/hooks', {
         let { data } = await instance.patch('/repos/liuzemei/git_hook_test/hooks/176880231', {
             config: {
-                url: 'http://118.24.121.133:9089/webhook'
-                // content_type: 'json'
+                url: 'http://118.24.121.133:9089/webhook',
+                content_type: 'json',
+                // secret: 
             }
         })
         console.log(data)
